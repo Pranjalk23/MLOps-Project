2336b95edc79a906becbadfa02136159e108cc55
Pranjal <prankashyap23@gmail.com>
2025-03-21 03:48:59 +0530

    Project structure ready, venv-set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pranjal kashyap",
+    author_email="prankashyap23@gmail.com",
+    packages=find_packages()
+)   
