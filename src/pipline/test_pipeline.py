56d9f950ae123dd619ab259e3efd0e8deafd1f64
Pranjal <prankashyap23@gmail.com>
2025-03-23 13:17:16 +0530

    Data Ingestion added

---
+++ src/pipline/test_pipeline.py
@@ -0,0 +1,10 @@
+class TestPipeline:
+    def __init__(self):
+        pass
+
+    def run_test(self):
+        print("Test pipeline running")
+
+if __name__ == "__main__":
+    test_pipeline = TestPipeline()
+    test_pipeline.run_test() 
